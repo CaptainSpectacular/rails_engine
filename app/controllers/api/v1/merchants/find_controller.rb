4f09164d28da66d958017132f47f7abcabec4896
pollockcl <pollock.clee@gmail.com>
2018-05-01 07:48:36 -0600

    refactored some of the logic down to the model level

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,19 +1,9 @@
 class Api::V1::Merchants::FindController < ApplicationController
   def show
-    case 
-    when params[:id]   then render json: Merchant.find(params[:id])
-    when params[:name] then render json: Merchant.find_by(name: params[:name])
-    when params[:created_at] then render json: Merchant.find_by(created_at: params[:created_at])
-    when params[:updated_at] then render json: Merchant.find_by(updated_at: params[:updated_at])
-    end
+    render json: Merchant.search(params)
   end
 
   def index
-    case
-    when params[:id]         then render json: Merchant.where(id: params[:id])
-    when params[:name]       then render json: Merchant.where(name: params[:name])
-    when params[:created_at] then render json: Merchant.where(created_at: params[:created_at])
-    when params[:updated_at] then render json: Merchant.where(updated_at: params[:updated_at])
-    end
+    render json: Merchnat.search_all(params)    
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,22 @@ class Merchant < ApplicationRecord
   def self.random
     order('random()').limit(1)
   end
+
+  def self.search(params)
+    case 
+    when params[:id]         then Merchant.find(params[:id])
+    when params[:name]       then Merchant.find_by(name: params[:name])
+    when params[:created_at] then Merchant.find_by(created_at: params[:created_at])
+    when params[:updated_at] then Merchant.find_by(updated_at: params[:updated_at])
+    end
+  end
+
+  def self.search_all(params)
+    case
+    when params[:id]         then Merchant.where(id: params[:id])
+    when params[:name]       then Merchant.where(name: params[:name])
+    when params[:created_at] then Merchant.where(created_at: params[:created_at])
+    when params[:updated_at] then Merchant.where(updated_at: params[:updated_at])
+    end
+  end
 end
