456a13a9904f4a534780fc5afb325f23303298eb
alexcutschall <alexander.cutschall@gmail.com>
2018-05-01 05:59:48 -0600

    adding in test for invoice relationships

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   gem 'pry'
   gem 'database_cleaner'
   gem 'factory_bot_rails'
+  gem 'shoulda-matchers', '~> 3.0'
 end
 
 group :development do
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-
+  describe "relationships" do
+    context "should belong to a merchant" do
+      it { should belong_to(:merchant) }
+      it { should belong_to(:customer) }
+    end
+  end
 end
