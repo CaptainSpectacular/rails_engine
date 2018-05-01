faedc10c1f39dad303eaa0a3b237463ac8e17ac2
pollockcl <pollock.clee@gmail.com>
2018-05-01 14:14:41 -0600

    fixed tests broken by serialization

--- spec/requests/transaction_spec.rb
+++ spec/requests/transaction_spec.rb
@@ -6,8 +6,8 @@ describe 'endpoints' do
     get '/api/v1/transactions'
 
     expect(response).to be_success
-    expect(JSON.parse(response.body).first).to eq(JSON.parse(t1.to_json)) 
-    expect(JSON.parse(response.body)[1]).to eq(JSON.parse(t2.to_json))
+    expect(JSON.parse(response.body).first['id']).to eq(t1.id) 
+    expect(JSON.parse(response.body)[1]['id']).to eq(t2.id)
   end
 
   it 'has show' do
@@ -15,15 +15,15 @@ describe 'endpoints' do
     get "/api/v1/transactions/#{transaction.id}"
 
     expect(response).to be_success
-    expect(JSON.parse(response.body)).to eq(JSON.parse(transaction.to_json))
+    expect(JSON.parse(response.body)['id']).to eq(transaction.id)
   end
 
   it 'can find by id' do
     t1 = create(:transaction)
     get "/api/v1/transactions/find?id=#{t1.id}"
 
-    expected = JSON.parse(t1.to_json)
-    anticipated = JSON.parse(response.body)
+    expected = t1.id 
+    anticipated = JSON.parse(response.body)['id']
 
     expect(response).to be_success
     expect(anticipated).to eq(expected)
@@ -33,8 +33,8 @@ describe 'endpoints' do
     t1 = create(:transaction)
     get "/api/v1/transactions/find?invoice_id=#{t1.invoice_id}"
 
-    expected = JSON.parse(t1.to_json)
-    anticipated = JSON.parse(response.body)
+    expected = t1.id
+    anticipated = JSON.parse(response.body)['id']
 
     expect(response).to be_success
     expect(anticipated).to eq(expected)
@@ -44,19 +44,19 @@ describe 'endpoints' do
     t1 = create(:transaction)
     get "/api/v1/transactions/find?credit_card_number=#{t1.credit_card_number}"
 
-    expected = JSON.parse(t1.to_json)
-    anticipated = JSON.parse(response.body)
+    expected = t1.id 
+    anticipated = JSON.parse(response.body)['id']
 
     expect(response).to be_success
     expect(anticipated).to eq(expected)
   end
 
   it 'can find by credit_card_number' do
-     t1 = create(:transaction)
+    t1 = create(:transaction)
     get "/api/v1/transactions/find?result=#{t1.result}"
 
-    expected = JSON.parse(t1.to_json)
-    anticipated = JSON.parse(response.body)
+    expected = t1.id 
+    anticipated = JSON.parse(response.body)['id']
 
     expect(response).to be_success
     expect(anticipated).to eq(expected)
@@ -66,8 +66,8 @@ describe 'endpoints' do
     t1 = create(:transaction)
     get "/api/v1/transactions/find?created_at=#{t1.created_at}"
 
-    expected = JSON.parse(t1.to_json)
-    anticipated = JSON.parse(response.body)
+    expected = t1.id 
+    anticipated = JSON.parse(response.body)['id']
 
     expect(response).to be_success
     expect(anticipated).to eq(expected)
@@ -78,8 +78,8 @@ describe 'endpoints' do
     t1 = create(:transaction)
     get "/api/v1/transactions/find?updated_at=#{t1.updated_at}"
     
-    expected = JSON.parse(t1.to_json)
-    anticipated = JSON.parse(response.body)
+    expected = t1.id 
+    anticipated = JSON.parse(response.body)['id']
 
     expect(response).to be_success
     expect(anticipated).to eq(expected)
@@ -87,15 +87,13 @@ describe 'endpoints' do
 
   it 'can find all by id' do
     t1 = create(:transaction)
-    get "/api/v1/transactions/find?updated_at=#{t1.updated_at}"
+    get "/api/v1/transactions/find_all?updated_at=#{t1.updated_at}"
     
-    expected = JSON.parse(t1.to_json)
-    anticipated = JSON.parse(response.body)
+    expected = t1.id 
+    anticipated = JSON.parse(response.body)['id']
 
     expect(response).to be_success
     expect(anticipated).to eq(expected)
-
-
   end
 end
 
