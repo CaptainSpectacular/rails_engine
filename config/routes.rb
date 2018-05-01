26b4ffc19e282ed0de690b6992b13413118cc8e7
alexcutschall <alexander.cutschall@gmail.com>
2018-05-01 08:20:49 -0600

    adding in completed item endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,14 @@ Rails.application.routes.draw do
       end
       resources :items, only: [:index, :show]
       resources :merchants, only: [:index, :show]
+      
+      namespace :items do
+        get '/find', to: 'find#show'
+        get 'find_all', to: 'find#index'
+        get 'random', to: 'random#show'
+      end
       resources :invoices, only: [:index, :show]
+      resources :items, only: [:index, :show]
     end
   end
 end
