ca189b745b9a68c073c5f39726277476837a7b41
alexcutschall <alexander.cutschall@gmail.com>
2018-05-02 07:53:27 -0600

    adding in messed up

--- app/controllers/api/v1/invoice_items/find_controller.rb
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -1,16 +1,6 @@
 class Api::V1::InvoiceItems::FindController < ApplicationController
   def index
-    if params[:id]
-      render json: InvoiceItem.where(id: params[:id])
-    elsif params[:quantity]
-      render json: InvoiceItem.where(quantity: params[:quantity])
-    elsif params[:unit_price]
-      render json: InvoiceItem.where(unit_price: params[:unit_price])
-    elsif params[:created_at]
-      render json: InvoiceItem.where(created_at: params[:created_at])
-    elsif params[:updated_at]
-      render json: InvoiceItem.where(updated_at: params[:updated_at])
-    end
+    render json: InvoiceItem.where(invoice_item_params)
   end
 
   def show
