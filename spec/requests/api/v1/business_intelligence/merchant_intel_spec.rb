4b98ce58a24f26da1c302a105f611e1f421fa6e2
alexcutschall <alexander.cutschall@gmail.com>
2018-05-02 17:04:55 -0600

    adding in two business intels

--- spec/requests/api/v1/business_intelligence/merchant_intel_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchant_intel_spec.rb
@@ -5,4 +5,9 @@ describe "Merchant Intel" do
     #most number of successful transactions
     get "/api/v1/merchants/#{merchant.id}/favorite_customer"
   end
+  it "can return the top number of merchants depending on revenue" do
+    x = 5
+    get "/api/v1/items/most_revenue?quantity=#{x}"
+  end
+
 end
