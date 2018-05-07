e10105f292673781961e6d7d84c00d0455346f67
pollockcl <pollock.clee@gmail.com>
2018-05-07 06:33:48 -0600

    added tests for invoice model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Invoice, type: :model do
   describe "relationships" do
     context "should belong to a merchant" do
       it { should belong_to(:merchant) }
+      it { should belong_to(:customer) }
+      it { should have_many(:transactions) }
+      it { should have_many(:invoice_items) }
+      it { should have_many(:transactions) }
     end
   end
 end
