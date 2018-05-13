6d4fd17b21655a8817c5f5187e61e368f384dd5c
pollockcl <pollock.clee@gmail.com>
2018-05-12 18:18:37 -0600

    added relationship tests for the item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-
+  describe 'relationships' do
+    it { is_expected.to belong_to(:merchant) }
+    it { is_expected.to have_many(:invoice_items) }    
+    it { is_expected.to have_many(:invoices) }    
+    it { is_expected.to have_many(:transactions) }    
+  end
 end
