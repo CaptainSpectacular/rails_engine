b5691a287e6641b2a5ea97c19e504aa2c927c518
pollockcl <pollock.clee@gmail.com>
2018-05-12 18:15:53 -0600

    added model test for customer #favorite_merchant

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { is_expected.to have_many(:invoices) }
+    it { is_expected.to have_many(:transactions) }
+    it { is_expected.to have_many(:merchants) }
+  end
+  
+  describe 'instance_methods 'do
+    it '#favorite_merchant' do
+      customer = create(:customer)
+      m1, m2   = create_list(:merchant, 2)
+      m1_invoice = create(:invoice, merchant: m1, customer: customer)
+      m2_invoice1 = create(:invoice, merchant: m2, customer: customer)
+      m2_invoice2 = create(:invoice, merchant: m2, customer: customer)
+      create(:transaction, invoice: m1_invoice)
+      create(:transaction, invoice: m2_invoice1)
+      create(:transaction, invoice: m2_invoice2)
+
+      expect(customer.favorite_merchant).to eq(m2)
+    end
+  end
 end
