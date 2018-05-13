0866a90846842ce2b5431c9efcd0174b6e0dd586
pollockcl <pollock.clee@gmail.com>
2018-05-12 18:26:13 -0600

    added model test on item for #most_revenue

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Item, type: :model do
     it { is_expected.to have_many(:invoices) }    
     it { is_expected.to have_many(:transactions) }    
   end
+
+  describe 'class methods' do
+    it '.most_revenue' do
+      merchant = create(:merchant)
+      customer = create(:customer)      
+      item1    = create(:item, merchant: merchant)
+      item2    = create(:item, merchant: merchant)
+      invoice1 = create(:invoice, merchant: merchant, customer: customer)
+      invoice2 = create(:invoice, merchant: merchant, customer: customer)
+      create(:transaction, invoice: invoice1)
+      create(:transaction, invoice: invoice2)
+      create(:transaction, invoice: invoice2)
+      create(:invoice_item, invoice: invoice1, item: item1)
+      create(:invoice_item, invoice: invoice2, item: item2)
+
+      expect(Item.most_revenue).to eq([item2, item1])
+      expect(Item.most_revenue(1)).to eq([item2])
+    end
+  end
 end
